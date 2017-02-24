683640a2c7bdb4bc1f3266445d8bdc049eb0e38d
likhona7 <likhona7>
2017-02-24 16:58:01 -0500

    bcrypt issue sorted

---
+++ collections/recipes.js
@@ -0,0 +1,109 @@
+Recipes = new Meteor.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+  name: {
+    type : String,
+    label: "Name"
+  },
+  desc: {
+    type: String,
+    label: "Description"
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function(){
+      return this.userId
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: "created At",
+    autoValue: function(){
+      return new Date()
+    }
+  }
+})
+
+Recipes.attachSchema( RecipeSchema );
+
+// if (Meteor.isServer) {
+//   // This code only runs on the server
+//   Meteor.publish('recipes', function tasksPublication() {
+//     return Tasks.find();
+//   });
+// }
+// // Recipes.allow({
+//     insert: function(userId, doc) {
+//         return !!userId;
+//     },
+//     update: function(userId, doc) {
+//         return !!userId;
+//     }
+// });
+//
+// Ingredient = new SimpleSchema({
+//     name: {
+//         type: String
+//     },
+//     amount: {
+//         type: String
+//     }
+// });
+//
+// RecipeSchema = new SimpleSchema({
+//     name: {
+//         type: String,
+//         label: "Name"
+//     },
+//     desc: {
+//         type: String,
+//         label: "Description"
+//     },
+//     ingredients: {
+//         type: [Ingredient]
+//     },
+//     inMenu: {
+//         type: Boolean,
+//         defaultValue: false,
+//         optional: true,
+//         autoform: {
+//             type: "hidden"
+//         }
+//     },
+//     author: {
+//         type: String,
+//         label: "Author",
+//         autoValue: function() {
+//             return this.userId
+//         },
+//         autoform: {
+//             type: "hidden"
+//         }
+//     },
+//     createdAt: {
+//         type: Date,
+//         label: "Created At",
+//         autoValue: function() {
+//             return new Date()
+//         },
+//         autoform: {
+//             type: "hidden"
+//         }
+//     }
+// });
+//
+// Meteor.methods({
+//     toggleMenuItem: function(id, currentState) {
+//         Recipes.update(id, {
+//             $set: {
+//                 inMenu: !currentState
+//             }
+//         });
+//     },
+//     deleteRecipe: function(id) {
+//         Recipes.remove(id);
+//     }
+// });
+//
+// Recipes.attachSchema(RecipeSchema);
